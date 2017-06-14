e01b37769b9bc35573a5553309d7c1f0e8b621ce
alshimaa14 <alshimaa.adel11@gmail.com>
2017-06-14 15:26:23 +0200

    go to product page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,6 @@ import { ProfilePage } from '../profile/profile';
 import { CategoriesPage } from '../categories/categories';
 import { CartPage } from "../cart/cart";
 import { OrderHistoryPage } from "../order-history/order-history";
-import { ScanBarCodePage } from "../scan-bar-code/scan-bar-code";
 import { BarcodeScanner,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 import { ProductService } from "../../Services/product.service";
 import { CartProductsService } from "../../Services/cartProducts.sevice";
@@ -18,57 +17,58 @@ import { ProductPage } from "../product/product";
 export class HomePage {
 
   options: BarcodeScannerOptions;
-  result: {};
-  barcode=11111;
+  result;
+  barcode;
 
   constructor(private toastCtrl: ToastController,private productService:ProductService,public cartProductsService:CartProductsService,private barCode:BarcodeScanner,public navCtrl: NavController,public navParams:NavParams) {
+    this.result={"text":"","format":"","cancelled":false};
+  }
+
+  async scanBarCode() {
+
+    this.options = {
+      prompt: 'Scan the bar code to see the result'
+    }
+
+    this.result = await this.barCode.scan(this.options);
     
-  }
+    if(this.productService.allProducts.length == 0) {
+      console.log("length is 0");
+    } else {
+      for(var i=0 ; i<this.productService.allProducts.length;i++) {  
+        console.log("text "+this.result.text);
+        console.log("db "+this.productService.allProducts[i].barcode)
+        if(this.result.text == this.productService.allProducts[i].barcode) {
+            this.navCtrl.push(ProductPage,{
+              "productId":this.productService.allProducts[i].idproduct,
+              "productName":this.productService.allProducts[i].name
+            })
+        break;
+      } else {
+        if(i==this.productService.allProducts.length-1){
+              let toast = this.toastCtrl.create({
+            message: 'Product is not exist',
+            duration: 3000,
+            position: 'bottom'
+          });
 
-  // async scanBarCode() {
-
-  //   this.options = {
-  //     prompt: 'Scan the bar code to see the result'
-  //   }
-
-  //   this.result = await this.barCode.scan(this.options);
-  //   console.log(this.result);
-  //   if(this.productService.allProducts.length == 0) {
-  //     console.log("length is 0");
-  //   } else {
-  //     for(var i=0 ; i<this.productService.allProducts.length;i++) {  
-        
-  //       if(this.barcode == this.productService.allProducts[i].barcode) {
-  //           this.navCtrl.push(ProductPage,{
-  //             "productId":this.productService.allProducts[i].idproduct,
-  //             "productName":this.productService.allProducts[i].name
-  //           })
-  //       break;
-  //     } else {
-  //       if(i==this.productService.allProducts.length-1){
-  //             let toast = this.toastCtrl.create({
-  //           message: 'Product is not exist',
-  //           duration: 3000,
-  //           position: 'bottom'
-  //         });
-
-  //     toast.onDidDismiss(() => {
-  //       console.log('Dismissed toast');
-  //     });
-  //     toast.present();
-  //       } else {
-  //       continue;
-  //       }
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+      });
+      toast.present();
+        } else {
+        continue;
+        }
       
-  //   }
-  // }
-  // }
-
-  // }
-
-  scanBarCode() {
-    this.navCtrl.push(ScanBarCodePage);
+    }
   }
+  }
+
+  }
+
+  // scanBarCode() {
+  //   this.navCtrl.push(ScanBarCodePage);
+  // }
 
   showCategories() {
     this.navCtrl.push(CategoriesPage);
