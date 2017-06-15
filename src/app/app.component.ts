cab99f4978fa2de747233bb3f4175c392a682ea4
alshimaa14 <alshimaa.adel11@gmail.com>
2017-06-15 12:44:15 +0200

    setRoot pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,13 +52,12 @@ export class MyApp {
 
   logOut() {
     this.storage.remove('email');
-    console.log(this.user_email);
-    this.nav.push(LoginPage);
+    this.nav.setRoot(LoginPage);
     this.setSideMenue();
   }
 
   logIn() {
-    this.nav.push(LoginPage);
+    this.nav.setRoot(LoginPage);
     this.setSideMenue();
   }
 
@@ -74,6 +73,6 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.push(page.component);
+    this.nav.setRoot(page.component);
   }
 }
